65ec480aa0b903cf7fe142cdb77524c1d4125cf3
Yousuf Jawwad <yousuf@maher.pk>
2023-09-25 03:09:38 +0500

    feat(workflows): added functionality to handle signals

--- queues/errors.go
+++ queues/errors.go
@@ -7,6 +7,10 @@ import (
 var (
 	// ErrClientNil is returned when the temporal client is nil.
 	ErrClientNil = errors.New("client is nil")
+
 	// ErrChildWorkflowExecutionAttempt is returned when attempting to execute a child workflow without the parent.
 	ErrChildWorkflowExecutionAttempt = errors.New("attempting to execute child workflow directly. use ExecuteWorkflow instead")
+
+	// ErrExternalWorkflowSignalAttempt is returned when attempting to signal an external workflow from within a workflow.
+	ErrExternalWorkflowSignalAttempt = errors.New("attempting to signal external workflow directly. use SignalExternalWorkflow instead")
 )
